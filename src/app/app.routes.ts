762ecc1f1c8723bdceafd54428b88315cc5681fd
Lionel <lourenco.lionel@hotmail.fr>
2026-04-14 19:03:05 +0200

    feat: added interface for space invaders

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SpaceInvader } from './space-invader/space-invader';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "space-invaders",
+        component: SpaceInvader
+    }
+];
---
+++ src/app/space-invader/space-invader.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-space-invader',
+  imports: [],
+  templateUrl: './space-invader.html',
+  styleUrl: './space-invader.scss',
+})
+export class SpaceInvader {}
