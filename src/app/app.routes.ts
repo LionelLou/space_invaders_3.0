3ffa0c43e98bce99c6f47ccbe2dfc2323935697a
Lionel <lourenco.lionel@hotmail.fr>
2026-04-15 05:05:20 +0200

    fix: removed docs folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { SpaceInvader } from './space-invader/space-invader';
 
 export const routes: Routes = [
     {
-        path: "/home",
+        path: "",
         component: SpaceInvader
     }
 ];
